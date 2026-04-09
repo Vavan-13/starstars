9521960a62187c2e04855d38bb9eb460aeb215bd
Evan Puthomme-Rostane <evan.pr13@gmail.com>
2026-04-09 10:59:13 +0200

    MAJ

--- sketch.js
+++ sketch.js
@@ -1,71 +1,47 @@
-const COULEURS = [
-  "#FF0000",
-  "#FF7F00",
-  "#FFFF00",
-  "#00FF00",
-  "#0000FF",
-  "#4B0082",
-  "#9400D3",
-];
+const CONFIG = {
+  systeme: {
+    nombreParticules: 1000,
+    couleurs: [
+      "#FF0000",
+      "#FF7F00",
+      "#FFFF00",
+      "#00FF00",
+      "#0000FF",
+      "#4B0082",
+      "#9400D3",
+    ],
+    couleurFond: [10, 15, 30, 50],
+  },
+  physique: {
+    vitesseNormale: 2,
+    vitesseAccelere: 50,
+    inertieAcceleration: 0.05,
+    inertieDeceleration: 0.1,
+  },
+  rendu: {
+    tailleMaxParticule: 12,
+  },
+};
 
-// Classe principale gérant le système
-class SystemeStellaire {
-  constructor(nombreParticules) {
-    this.nombreParticules = nombreParticules;
-    this.particules = [];
-    this.vitesse = 2;
-  }
-
-  // Méthode d'initialisation appelée au début (ou pour reset)
-  init() {
-    this.particules = [];
-    for (let i = 0; i < this.nombreParticules; i++) {
-      this.particules.push(new Particule());
-      // On randomise le Z initial pour éviter qu'elles partent toutes en même temps
-      this.particules[i].z = random(width);
-    }
-  }
-
-  // Mise à jour de la logique (ex: gestion de la vitesse avec la souris)
-  update(interaction) {
-    if (interaction) {
-      this.vitesse = lerp(this.vitesse, 50, 0.05);
-    } else {
-      this.vitesse = lerp(this.vitesse, 2, 0.1);
-    }
-  }
-
-  // Affichage du système complet
-  display() {
-    background(10, 15, 30, 50);
-
-    push(); // Isole les transformations
-    translate(width / 2, height / 2);
-
-    for (let p of this.particules) {
-      p.update(this.vitesse);
-      p.display(this.vitesse);
-    }
-
-    pop();
-  }
-}
-
-// Classe gérant une particule individuelle
 class Particule {
+  #couleur;
+  #pz; // Position Z précédente pour dessiner la traînée
+
   constructor() {
-    this.reset();
+    this.reset(true);
   }
 
-  // Réinitialise la particule quand elle dépasse la caméra
-  reset() {
+  reset(premierLancement = false) {
     this.x = random(-width, width);
     this.y = random(-height, height);
-    this.z = width;
-    this.couleur = random(COULEURS);
+    this.z = premierLancement ? random(width) : width; // Répartit la profondeur au départ
+
+    this.#pz = this.z;
+    this.#couleur = random(CONFIG.systeme.couleurs);
   }
 
   update(vitesseGlobale) {
+    this.#pz = this.z;
     this.z -= vitesseGlobale;
 
     if (this.z < 1) {
@@ -73,39 +49,79 @@ class Particule {
     }
   }
 
-  display(vitesseGlobale) {
+  display() {
     let sx = map(this.x / this.z, 0, 1, 0, width);
     let sy = map(this.y / this.z, 0, 1, 0, height);
-    let r = map(this.z, 0, width, 12, 0);
+    let rayon = map(this.z, 0, width, CONFIG.rendu.tailleMaxParticule, 0);
+    let px = map(this.x / this.#pz, 0, 1, 0, width);
+    let py = map(this.y / this.#pz, 0, 1, 0, height);
 
-    let px = map(this.x / (this.z + vitesseGlobale), 0, 1, 0, width);
-    let py = map(this.y / (this.z + vitesseGlobale), 0, 1, 0, height);
-
-    stroke(this.couleur);
-    strokeWeight(r);
+    stroke(this.#couleur);
+    strokeWeight(rayon);
     line(px, py, sx, sy);
   }
 }
 
-// --- Fonctions p5.js ---
+class SystemeStellaire {
+  #particules;
+  #vitesseActuelle;
+
+  constructor() {
+    this.#particules = [];
+    this.#vitesseActuelle = CONFIG.physique.vitesseNormale;
+  }
+
+  init() {
+    this.#particules = [];
+    for (let i = 0; i < CONFIG.systeme.nombreParticules; i++) {
+      this.#particules.push(new Particule());
+    }
+  }
+
+  update(estEnInteraction) {
+    const cible = estEnInteraction
+      ? CONFIG.physique.vitesseAccelere
+      : CONFIG.physique.vitesseNormale;
+
+    const lerpForce = estEnInteraction
+      ? CONFIG.physique.inertieAcceleration
+      : CONFIG.physique.inertieDeceleration;
+
+    this.#vitesseActuelle = lerp(this.#vitesseActuelle, cible, lerpForce);
+    for (let particule of this.#particules) {
+      particule.update(this.#vitesseActuelle);
+    }
+  }
+
+  display() {
+    background(...CONFIG.systeme.couleurFond);
+
+    push();
+    translate(width / 2, height / 2);
+
+    for (let particule of this.#particules) {
+      particule.display();
+    }
+
+    pop();
+  }
+}
 
 let systeme;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  // Instanciation et initialisation via la méthode init()
-  systeme = new SystemeStellaire(1000);
+  systeme = new SystemeStellaire();
   systeme.init();
 }
 
 function draw() {
-  // On passe mouseIsPressed à l'update pour gérer l'interaction
   systeme.update(mouseIsPressed);
   systeme.display();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  // Optionnel : on pourrait appeler systeme.init() ici si on veut réinitialiser l'effet au redimensionnement
+  // Décommente si on veux recréer les particules au redimensionnement
+  // systeme.init();
 }
