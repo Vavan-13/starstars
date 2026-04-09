2152840342933856ba8a6a27f0720adb5c28a66f
Evan Puthomme-Rostane <evan.pr13@gmail.com>
2026-04-09 11:56:28 +0200

    maj

--- sketch.js
+++ sketch.js
@@ -10,118 +10,131 @@ const CONFIG = {
       "#4B0082",
       "#9400D3",
     ],
-    couleurFond: [10, 15, 30, 50],
+    fond: [10, 15, 30, 50],
   },
   physique: {
-    vitesseNormale: 2,
-    vitesseAccelere: 50,
-    inertieAcceleration: 0.05,
-    inertieDeceleration: 0.1,
+    vitesseBase: 2,
+    vitesseBoost: 50,
+    acceleration: 0.05,
+    deceleration: 0.1,
   },
   rendu: {
-    tailleMaxParticule: 12,
+    tailleMax: 120, // taille des cercles
   },
 };
 
 class Particule {
   #couleur;
-  #pz; // Position Z précédente pour dessiner la traînée
+  #pz;
 
   constructor() {
     this.reset(true);
   }
 
-  reset(premierLancement = false) {
-    this.x = random(-width, width);
-    this.y = random(-height, height);
-    this.z = premierLancement ? random(width) : width; // Répartit la profondeur au départ
-
+  reset(isInitial = false) {
+    // Spawn large pour couvrir les mouvements du point de fuite
+    this.x = random(-width * 1.5, width * 1.5);
+    this.y = random(-height * 1.5, height * 1.5);
+    this.z = isInitial ? random(width) : width;
     this.#pz = this.z;
     this.#couleur = random(CONFIG.systeme.couleurs);
   }
 
-  update(vitesseGlobale) {
+  update(vitesse) {
     this.#pz = this.z;
-    this.z -= vitesseGlobale;
-
-    if (this.z < 1) {
-      this.reset();
-    }
+    this.z -= vitesse;
+    if (this.z < 1) this.reset();
   }
 
-  display() {
-    let sx = map(this.x / this.z, 0, 1, 0, width);
-    let sy = map(this.y / this.z, 0, 1, 0, height);
-    let rayon = map(this.z, 0, width, CONFIG.rendu.tailleMaxParticule, 0);
-    let px = map(this.x / this.#pz, 0, 1, 0, width);
-    let py = map(this.y / this.#pz, 0, 1, 0, height);
+  draw() {
+    // Projection 3D vers 2D
+    const sx = map(this.x / this.z, 0, 1, 0, width);
+    const sy = map(this.y / this.z, 0, 1, 0, height);
+    const px = map(this.x / this.#pz, 0, 1, 0, width);
+    const py = map(this.y / this.#pz, 0, 1, 0, height);
+
+    const r = map(this.z, 0, width, CONFIG.rendu.tailleMax, 0);
 
     stroke(this.#couleur);
-    strokeWeight(rayon);
+    strokeWeight(r);
     line(px, py, sx, sy);
   }
 }
 
-class SystemeStellaire {
-  #particules;
-  #vitesseActuelle;
+class MoteurStellaire {
+  #particules = [];
+  #vitesse = CONFIG.physique.vitesseBase;
+  #origine;
 
-  constructor() {
-    this.#particules = [];
-    this.#vitesseActuelle = CONFIG.physique.vitesseNormale;
+  constructor(nb) {
+    this.#origine = createVector(width / 2, height / 2);
+    for (let i = 0; i < nb; i++) this.#particules.push(new Particule());
   }
 
-  init() {
-    this.#particules = [];
-    for (let i = 0; i < CONFIG.systeme.nombreParticules; i++) {
-      this.#particules.push(new Particule());
-    }
+  update(isPressed) {
+    // Mise à jour de l'origine uniquement au clic
+    if (isPressed) this.#origine.set(mouseX, mouseY);
+
+    const cible = isPressed
+      ? CONFIG.physique.vitesseBoost
+      : CONFIG.physique.vitesseBase;
+    const force = isPressed
+      ? CONFIG.physique.acceleration
+      : CONFIG.physique.deceleration;
+
+    this.#vitesse = lerp(this.#vitesse, cible, force);
+    this.#particules.forEach((p) => p.update(this.#vitesse));
   }
 
-  update(estEnInteraction) {
-    const cible = estEnInteraction
-      ? CONFIG.physique.vitesseAccelere
-      : CONFIG.physique.vitesseNormale;
-
-    const lerpForce = estEnInteraction
-      ? CONFIG.physique.inertieAcceleration
-      : CONFIG.physique.inertieDeceleration;
-
-    this.#vitesseActuelle = lerp(this.#vitesseActuelle, cible, lerpForce);
-    for (let particule of this.#particules) {
-      particule.update(this.#vitesseActuelle);
-    }
-  }
-
-  display() {
-    background(...CONFIG.systeme.couleurFond);
-
+  render() {
+    background(...CONFIG.systeme.fond);
     push();
-    translate(width / 2, height / 2);
-
-    for (let particule of this.#particules) {
-      particule.display();
-    }
-
+    translate(this.#origine.x, this.#origine.y);
+    this.#particules.forEach((p) => p.draw());
     pop();
   }
 }
 
-let systeme;
+class Aura {
+  draw(isActive) {
+    const x = mouseX || width / 2;
+    const y = mouseY || height / 2;
+
+    push();
+    translate(x, y);
+    noStroke();
+
+    if (isActive) {
+      fill(100, 200, 255, 40);
+      circle(0, 0, 100);
+      fill(255, 150);
+      circle(0, 0, 10);
+    } else {
+      const pulse = sin(frameCount * 0.1) * 5;
+      fill(255, 20);
+      circle(0, 0, 30 + pulse);
+      fill(255, 100);
+      circle(0, 0, 4);
+    }
+    pop();
+  }
+}
+
+let moteur, interfaceUtilisateur;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  systeme = new SystemeStellaire();
-  systeme.init();
+  moteur = new MoteurStellaire(CONFIG.systeme.nombreParticules);
+  interfaceUtilisateur = new Aura();
+  noCursor();
 }
 
 function draw() {
-  systeme.update(mouseIsPressed);
-  systeme.display();
+  moteur.update(mouseIsPressed);
+  moteur.render();
+  interfaceUtilisateur.draw(mouseIsPressed);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  // Décommente si on veux recréer les particules au redimensionnement
-  // systeme.init();
 }
